7ea6c6b6863b1b38719c149f7a05fa3b2cad0062
Andri R <andrewflash@gmail.com>
2023-08-07 21:11:18 +0700

    Add MQTT ultrasonic

---
+++ source/mqtt/mqtt-ultrasonic-sensor.py
@@ -0,0 +1,82 @@
+from paho.mqtt import client as mqtt_client
+import time
+import random
+import RPi.GPIO as GPIO
+
+broker = 'broker.hivemq.com'
+port = 1883
+topic = "hoocage/sensor1"
+client_id = f'py-mqtt-{random.randint(0, 1000)}'
+
+#GPIO Mode (BOARD / BCM)
+GPIO.setmode(GPIO.BCM)
+ 
+#set GPIO Pins
+GPIO_TRIGGER = 23
+GPIO_ECHO = 24
+ 
+#set GPIO direction (IN / OUT)
+GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
+GPIO.setup(GPIO_ECHO, GPIO.IN)
+
+def connect_mqtt():
+    def on_connect(client, userdata, flags, rc):
+        if rc == 0:
+            print("Connected to MQTT Broker!")
+        else:
+            print("Failed to connect, return code %d\n", rc)
+    # Set Connecting Client ID
+    client = mqtt_client.Client(client_id)
+    client.on_connect = on_connect
+    client.connect(broker, port)
+    return client
+
+def publish(client):
+     while True:
+         dist = distance()
+         print ("Measured Distance = %.1f cm" % dist)
+         client.publish(topic, dist)
+         time.sleep(0.5)
+
+def distance():
+    # set Trigger to HIGH
+    GPIO.output(GPIO_TRIGGER, True)
+ 
+    # set Trigger after 0.01ms to LOW
+    time.sleep(0.00001)
+    GPIO.output(GPIO_TRIGGER, False)
+ 
+    StartTime = time.time()
+    StopTime = time.time()
+ 
+    # save StartTime
+    while GPIO.input(GPIO_ECHO) == 0:
+        StartTime = time.time()
+ 
+    # save time of arrival
+    while GPIO.input(GPIO_ECHO) == 1:
+        StopTime = time.time()
+ 
+    # time difference between start and arrival
+    TimeElapsed = StopTime - StartTime
+    # multiply with the sonic speed (34300 cm/s)
+    # and divide by 2, because there and back
+    distance = (TimeElapsed * 34300) / 2
+ 
+    return distance
+    
+def run():
+    client = connect_mqtt()
+    client.loop_start()
+    publish(client)
+    client.loop_stop()
+    
+if __name__ == '__main__':
+    try:
+        while True:   
+            run()
+    
+    # Reset by pressing CTRL + C
+    except KeyboardInterrupt:
+        print("Measurement stopped by User")
+        GPIO.cleanup()
